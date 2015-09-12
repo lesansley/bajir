f0de1455aae044f21add7d1120ef4575df254c16
brettansley <brett.ansley@thoughtworks.com>
2015-09-12 19:26:42 +0100

    Create edit, update and destroy methods

--- www/bajir/app/controllers/procedures_controller.rb
+++ www/bajir/app/controllers/procedures_controller.rb
@@ -12,6 +12,10 @@ class ProceduresController < ApplicationController
     	@procedure = Procedure.new
     end
 
+	def edit
+  		@procedure = Procedure.find(params[:id])
+	end
+
     def create
     	@procedure = Procedure.new(procedure_params)
     	if @procedure.save
@@ -20,6 +24,23 @@ class ProceduresController < ApplicationController
     		render 'new'
     	end
     end
+
+    def update
+  		@procedure = Procedure.find(params[:id])
+ 
+  		if @procedure.update(procedure_params)
+    		redirect_to @procedure
+  		else
+    		render 'edit'
+  		end
+	end
+
+	def destroy
+  		@procedure = Article.find(params[:id])
+  		@procedure.destroy
+ 
+  		redirect_to procedures_path
+	end
 
 private
 	def procedure_params
