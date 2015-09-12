298be199ca93b3aad5a128984f5f9b3313676497
brettansley <brett.ansley@thoughtworks.com>
2015-09-12 17:12:15 +0100

    Creating database model and show procedure

--- www/bajir/app/controllers/procedures_controller.rb
+++ www/bajir/app/controllers/procedures_controller.rb
@@ -1,4 +1,10 @@
 class ProceduresController < ApplicationController
     def new
     end
+
+    def create
+    	@procedure = Procedure.new(params.require(:procedure).permit(:tnumber, :procedureDate))
+    	@procedure.save
+    	redirect_to @procedure    
+    end
 end
