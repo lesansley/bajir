53b172fe1d6f7b664917dd3f335dd29313bba66d
Les Ansley <les@ansleyfamily.com>
2015-09-14 10:29:46 +0100

    delete associated questionnaires with deleted procedure

--- www/bajir/app/models/procedure.rb
+++ www/bajir/app/models/procedure.rb
@@ -1,5 +1,5 @@
 class Procedure < ActiveRecord::Base
-	has_many :questionnaires
+	has_many :questionnaires, dependent: :destroy
     validates :tnumber, presence: true,
 					length: { minimum: 5 }
 end
