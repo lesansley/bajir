531f16997215f57c3f74d065e3896b630add37ac
Les Ansley <les@ansleyfamily.com>
2015-09-14 09:37:49 +0100

    update questionnaire controller

--- www/bajir/app/controllers/questionnaires_controller.rb
+++ www/bajir/app/controllers/questionnaires_controller.rb
@@ -1,2 +1,12 @@
 class QuestionnairesController < ApplicationController
+    def create
+        @procedure = Procedure.find(params[:procedure_id])
+        @questionnaire = @procedure.questionnaires.create(questionnaire_params)
+        redirect_to procedure_path(@procedure)
+    end
+
+    private
+        def questionnaire_params
+        params.require(:questionnaire).permit(:username, :date, :score)
+    end
 end
