c66fc22d95604ed566b228eebd49be79c3ef80e4
Les Ansley <les@ansleyfamily.com>
2015-09-14 12:32:35 +0100

    Setting up survey template web pages

--- www/bajir/app/controllers/surveytemplates_controller.rb
+++ www/bajir/app/controllers/surveytemplates_controller.rb
@@ -1,2 +1,49 @@
 class SurveytemplatesController < ApplicationController
+    def index
+        @surveytemplates = Surveytemplate.all
+    end
+
+    def show
+        @surveytemplate = Surveytemplate.find(params[:id])
+    end
+
+    def new
+        @surveytemplate = Surveytemplate.new
+    end
+
+    def edit
+        @surveytemplate = Surveytemplate.find(params[:id])
+    end
+
+    def create
+        @surveytemplate = Surveytemplate.new(surveytemplate_params)
+        if @surveytemplate.save
+            redirect_to @surveytemplate
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @surveytemplate = Surveytemplate.find(params[:id])
+
+        if @surveytemplate.update(surveytemplate_params)
+            redirect_to @surveytemplate
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @surveytemplate = Surveytemplate.find(params[:id])
+        @surveytemplate.destroy
+
+        redirect_to surveytemplates_path
+    end
+
+private
+    def surveytemplate_params
+        params.require(:surveytemplate).permit(:name, :description)
+    end
+
 end
