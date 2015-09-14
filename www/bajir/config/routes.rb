50a0a60e0bea0eaccee34443e37c963a8c9f3eec
Les Ansley <les@ansleyfamily.com>
2015-09-14 09:48:11 +0100

    defined the route for questionnaire

--- www/bajir/config/routes.rb
+++ www/bajir/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   #create a resource
-  resources :procedures
+  resources :procedures do
+    resources :questionnaires
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
