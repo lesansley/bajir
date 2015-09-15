e85ac6ae2effad12748f1e08881aacc513b86850
brettansley <brett.ansley@thoughtworks.com>
2015-09-15 09:32:58 +0100

    Added pages and controllers for the questions

--- www/bajir/app/controllers/questions_controller.rb
+++ www/bajir/app/controllers/questions_controller.rb
@@ -1,2 +1,20 @@
 class QuestionsController < ApplicationController
+	def create
+		@surveytemplate = Surveytemplate.find(params[:surveytemplate_id])
+		@question = @surveytemplate.questions.create(question_params)
+		redirect_to edit_surveytemplate_path(@surveytemplate)
+	end
+
+  def destroy
+    @surveytemplate = Surveytemplate.find(params[:surveytemplate_id])
+    @question = @surveytemplate.questions.find(params[:id])
+    @question.destroy
+    redirect_to surveytemplate_path(@surveytemplate)
+  end
+
+	private 
+	def question_params
+		params.require(:question).permit(:question, :answerParameters)
+		
+	end
 end
